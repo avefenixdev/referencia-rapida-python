3382db1d61bf7f540b8a39989461c2e850a2f32e
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-16 14:42:14 -0300

    fix: usos típicos cadenas

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ print("¡Hola Mundo!")
 #? ---------------------
 
 nombre = "Maximiliano"
+segundo_nombre = "Luis"
+
 print(nombre)
-print(type(nombre))
+print(segundo_nombre)
+print(type(nombre)) # str
+print(type(segundo_nombre)) # str
+
+# * Operaciones básicas con cadenas
+
+# Concatenación
+nombre_segundo_nombre = nombre + " " + segundo_nombre
+
+# Uso típico
+print("Nombre y segundo nombre: ", nombre_segundo_nombre)
+
+# f-strings (forma correcta y moderna)
+
+print(f"Hola, mi nombre es '{nombre}' y mi segundo nombre es '{segundo_nombre}'")
+
 
 
