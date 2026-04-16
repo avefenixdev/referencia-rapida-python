a054398af446692672cb829a4a6ffc9f3346f7cc
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-16 15:38:08 -0300

    add: Operadores de asignación

--- operadores.py
+++ operadores.py
@@ -17,3 +17,17 @@ print(a // b) # División entera -> 11
 print(a % b) # Módulo (resto) -> 0
 print(a ** b) # Potencia -> 484
 
+#? -------------------------
+#? Operadores de asignación
+#? -------------------------
+
+x = 5  # asignación
+print(x) # 5
+x += 2 # x = x + 2 # incremento
+print(x) # 7
+x -= 1 # x = x - 1 # decremento
+print(x) # 6
+x *= 3 # x = x * 3 # multiplicación compuesta
+print(x) # 18
+x /= 2 # x = x / 2 # división compuesta
+print(x) # 9.0
