be66495aae109476cc5a1da7805cfd86850528d0
Ave Fenix Dev <avefenixdev@gmail.com>
2026-04-16 15:34:11 -0300

    add: Operadores aritméticos

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# ! ======================
+# ! Operadores
+# ! ======================
+
+#? -----------------------
+#? Operadores aritméticos
+#? -----------------------
+
+a = 22
+b = 2
+
+print(a + b) # Suma -> 24
+print(a - b) # Resta -> 20
+print(a * b) # Multiplicación -> 44
+print(a / b) # División (float) -> 11.0
+print(a // b) # División entera -> 11
+print(a % b) # Módulo (resto) -> 0
+print(a ** b) # Potencia -> 484
+
